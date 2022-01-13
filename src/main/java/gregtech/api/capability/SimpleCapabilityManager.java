1162068198a87bb1cfbbd725bdb2d56108589ebb
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-01-13 20:12:18 +0000

    change to IMultiblockAbilityPart<IItemHandlerModifiable> hatch

--- src/main/java/gregtech/api/capability/SimpleCapabilityManager.java
+++ src/main/java/gregtech/api/capability/SimpleCapabilityManager.java
@@ -49,8 +49,6 @@ public class SimpleCapabilityManager {
         registerCapabilityWithNoDefault(IMultipleRecipeMaps.class);
         registerCapabilityWithNoDefault(AbstractRecipeLogic.class);
         registerCapabilityWithNoDefault(HardwareProvider.class);
-        registerCapabilityWithNoDefault(IResearchDataProvider.class);
-
         registerCapabilityWithNoDefault(IWrenchItem.class);
         registerCapabilityWithNoDefault(ICutterItem.class);
         registerCapabilityWithNoDefault(IScrewdriverItem.class);
